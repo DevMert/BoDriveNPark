4648c6c06651a8a0dbe30b2bc1b24dcc8ceaafc5
Tura92 <daniel.tura@live.de>
2019-05-02 14:34:10 +0200

    Frisches Projekt

--- src/App.js
+++ src/App.js
@@ -1,92 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import InputTag from './components/InputTag';
-import Modal from './components/Modal/Modal';
 
-import withBorder from './components/HOC/withBorder';
-
-
-class App extends Component {
-
-  static init() {
-
-    App.box = document.getElementsByClassName('box')[0]
-
-    App.box.addEventListener("dragstart", App.dragstart)
-    App.box.addEventListener("dragend", App.dragend)
-
-    const containers = document.getElementsByClassName('holder')
-
-    for(const container of containers) {
-      container.addEventListener("dragover", App.dragover)
-      container.addEventListener("dragenter", App.dragenter)
-      container.addEventListener("dragleave", App.dragleave)
-      container.addEventListener("drop", App.drop)
-    }
-  }
-
-  static dragstart() {
-    this.className += " held"
-  
-    setTimeout(()=>this.className="invisible", 0)
-  }
-
-  static dragend() {
-    this.className = "box"
-  }
-
-  static dragover(e) {
-    e.preventDefault()
-  }
-
-  static dragenter(e) {
-    e.preventDefault()
-    this.className += " hovered"
-  }
-
-  static dragleave() {
-    this.className = "holder"
-  }
-
-  static drop() {
-    this.className = "holder"
-    this.append(App.box)
-  }
-
-
-
-
-  state = {
-    show: false,
-  }
-
-  showModal = () => {
-    this.setState({
-      ...this.state,
-      show: !this.state.show
-    });
-  }
-  render() {
-    let WithBorderInput = withBorder (InputTag);
-
-    return (
-      <div className="App">
-        {/* <WithBorderInput placeholder="press enter/space to + tag" /> */}
-
-        <input type="button"
-          onClick={this.showModal}
-          value="Show Modal" />
-
-        <Modal onClose={this.showModal} show={this.state.show}>
-              This message is from Modal!
-        </Modal>
-
-        
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World die wzeite</p>
+    </div>
+  );
 }
-document.addEventListener("DOMContentLoaded", App.init)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AppDragDropDemo from './AppDragDropDemo';
-import AppSlideShow from './AppSlideShow';
-import AppWithModal from './AppWithModal';
+import * as serviceWorker from './serviceWorker';
 
-import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<AppDragDropDemo />, document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
