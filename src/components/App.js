2043cad1cd7cdeb44fc7ad597119ff99d059f17b
Tura92 <daniel.tura@live.de>
2019-05-02 15:00:55 +0200

    NavBar Component erstellt

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import '../App.css';
+
+class App extends Component {
+  
+  render(){
+    return (
+      <div className="App">
+        <p>Hello World die wzeite</p>
+        <p>Und noch in test</p>
+      </div>
+    );
+  }
+    
+  
+  
+  
+}
+
+export default App;
