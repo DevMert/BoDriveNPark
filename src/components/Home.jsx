3d37adba996d7b366255f846a16bfeef87e963de
h1gg <M.Yalovac@icloud.com>
2019-05-13 14:22:33 +0200

    Navbar + altePlaene Komponente

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
-import './Home.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
+import "./Home.css";
+
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>;
 
 export default class Home extends Component {
   render() {
@@ -14,6 +21,6 @@ export default class Home extends Component {
           </Link>
         </Jumbotron>
       </Grid>
-    )
+    );
   }
 }
---
+++ src/components/oldPlans.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+
+class oldPlans extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+                
+
+
+
+
+         );
+    }
+}
+ 
+export default oldPlans;
