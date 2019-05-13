e97350920a6faa3112605d0da6573a4fd5638991
h1gg <M.Yalovac@icloud.com>
2019-05-13 17:11:09 +0200

    OldPlans zentriert

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import Header from "./NavBar";
 import Dashboard from "./Dashboard";
 import map from "./Map";
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import OldPlans from "./OldPlans";
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
-
+        <OldPlans />
         <Route exact path="/" component={Dashboard} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/map" component={map} />
---
+++ src/components/OldPlans.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Button, OverlayTrigger, Popover } from "react-bootstrap";
+
+export default class oldPlans extends Component {
+  render() {
+    return (
+      <div style={{ marginLeft: "550px", marginRight: "550px" }}>
+        <OverlayTrigger
+          trigger="click"
+          placement="left"
+          overlay={
+            <Popover id="popover-basic" title="Deine alten Pläne">
+              <Button variant="info">01.03-11.03 </Button>
+              <Button variant="info">10.03-22.03 </Button>
+              <Button variant="info">15.03-22.03 </Button>
+              <Button variant="info">21.03-25.03 </Button>
+              <Button variant="info">26.03-31.03 </Button>
+              <Button variant="info">02.04-07.04 </Button>
+            </Popover>
+          }
+        >
+          <Button variant="success">
+            <span class="glyphicon glyphicon-search" aria-hidden="true">
+              Alte Pläne aufrufen
+            </span>
+          </Button>
+        </OverlayTrigger>
+      </div>
+    );
+  }
+}
