f469050ec3caaeb6391b69b94057bd57c5e313a5
Daniel Tura <daniel.tura@live.de>
2019-06-04 13:09:10 +0200

    Algo läuft! Verbesserungsbedarf!

--- src/components/experimental/Experimental.jsx
+++ src/components/experimental/Experimental.jsx
@@ -9,8 +9,23 @@ class Parkplatz {
     this.MAX_Slots = MAX_Slots;
   }
 
+  getMax() {
+    return this.MAX_Slots;
+  }
+
   addUser(user) {
     this.users.push(user);
+  }
+
+  getUsers() {
+    let uArr = [];
+    this.users.forEach(userArr => {
+      Array.isArray(userArr)
+        ? userArr.forEach(user => uArr.push(user))
+        : uArr.push(userArr);
+    });
+
+    return uArr;
   }
 }
 
@@ -23,12 +38,6 @@ class Experimental extends Component {
    * "mo", "di", "mi", "do", "fr"
    */
   algo2 = tag => {
-    //Parkplätze inistialisieren
-    var HP = new Parkplatz(3);
-    var NP = new Parkplatz(1);
-    var HP_Index = 0;
-    var NP_Index = 0;
-
     //User initialisieren/fetchen
     var users = data.users;
 
@@ -39,41 +48,78 @@ class Experimental extends Component {
     });
 
     //User bekommen für Testzwecke zufällige Parkwerte
-    users.map(u => {
-      u.parkwert = Math.floor(Math.random() * 200) + 20;
-      console.log(
-        "User nr. " + u.matnr + " hat folgenden Parkwert: " + u.parkwert
-      );
-    });
+    // users.map(u => {
+    //   u.parkwert = Math.floor(Math.random() * 200) + 20;
+    //   console.log(
+    //     "User nr. " + u.matnr + " hat folgenden Parkwert: " + u.parkwert
+    //   );
+    // });
 
     /*User werden nach Parkwert sortiert*/
     users.sort(function(a, b) {
       return a.parkwert - b.parkwert;
     });
 
-    /*Parkplätze belegen*/
+    //Parkplätze inistialisieren
+    var HP = new Parkplatz(10);
+    var NP = new Parkplatz(1);
 
+    /*Parkplätze belegen*/
+    /**
+     * Jeder Parkplatz wird belegt. Anschließend werden nach jeder
+     * Belegung die User mit Parkfläche global aus dem Array gelöscht
+     */
+    this.fillParkplatz(users, HP, tag, "Hauptparkplatz");
+    HP.getUsers().forEach(user => {
+      users = this.deleteUser(users, user.matnr);
+    });
+
+    this.fillParkplatz(users, NP, tag, "Nebenparkplatz");
+    NP.getUsers().forEach(user => {
+      users = this.deleteUser(users, user.matnr);
+    });
+
+    console.log(HP);
+    console.log(NP);
+    console.log(users);
+  };
+  //------------------------------------------------------------------------
+
+  fillParkplatz(users, PP, tag, pref) {
     //In diesem Array werden die Zuweisungen an die
-    //Parkplätze gespeichert
-    var parkBuddiesHP = [];
+    //Parkflächen gespeichert
+    var parkBuddiesArr = [];
     //Array zum Zwischenspeichern
     var temp = [];
     //Anzahl an Suchvorgängen
     var loops = users.length;
 
     for (var i = 0; i < loops; i++) {
-      temp = this.fillSlots(users, tag, "Hauptparkplatz");
+      temp = this.getFilledSlots(users, tag, pref);
       if (temp !== undefined) {
-        parkBuddiesHP.push(temp);
+        parkBuddiesArr.push(temp);
+        //Lösche User lokal aus Array, damit diese nicht nochmal vergeben werden
         temp.forEach(user => (users = this.deleteUser(users, user.matnr)));
       }
     }
+    //console.log(users);
+    //Maximale Parkflächen auf Parkplatz definieren
+    var P_Index = 0;
 
-    console.log(parkBuddiesHP);
-  };
-  //------------------------------------------------------------------------
+    //Alle BuddieArrays auf den Parkplatz pushen, bis alle Plätze
+    //voll sind oder keine buddies mehr im Array
+    for (var j = 0; j < parkBuddiesArr.length; j++) {
+      if (P_Index >= PP.getMax()) break;
+      else {
+        PP.addUser(parkBuddiesArr[j]);
+        P_Index++;
+      }
+    }
 
-  fillSlots(users, tag, pref) {
+    return PP;
+  }
+
+  getFilledSlots(users, tag, pref) {
     var emptyPF = [
       false,
       false,
@@ -149,6 +195,14 @@ class Experimental extends Component {
     return users.filter(user => !(user.matnr === matnr));
   }
 
+  /**
+   * Prüft, ob Zeiten des Users zu den freien Zeit
+   * auf der Parkfläche passen
+   * @param {*} pArr
+   * Array der Parkfläche
+   * @param {*} uArr
+   * Array des Users
+   */
   checkAvail(pArr, uArr) {
     for (var i = 0; i < pArr.length; i++) {
       if (pArr[i] === true && uArr[i] === true) return false;
@@ -180,7 +234,7 @@ class Experimental extends Component {
           //...bis ein Element true zurück gibt
           if (flagArr[j] === true) {
             //Itteriere über alle Elemente zwischen
-            //i und j die false sind und setze diese auf true
+            //i und j und setze diese auf true
             for (var k = i; k <= j; k++) {
               flagArr[k] = true;
             }
