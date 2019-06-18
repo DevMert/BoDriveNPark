921c664916fe39a5dd813b4f13790fbeb9c377b0
Tura92 <daniel.tura@live.de>
2019-06-18 16:11:08 +0200

    Algorithmus funktioniert wieder

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Stundenplan from "./Praeferenzen/Stundenplan";
-import Reservierung from "./Reservierung/Reservierung";
-import Experimental from "./experimental/Experimental";
-import Karte from "./Karte/Karte";
-import Login from "./Login/Login";
+import Stundenplan from './Praeferenzen/Stundenplan';
+import Reservierung from './Reservierung/Reservierung';
+import Algorithmus from './experimental/Algorithmus';
+import Karte from './Karte/Karte';
+import Login from './Login/Login';
 
 function onAuthRequired({ history }) {
-  history.push("/login");
+  history.push('/login');
 }
 
 export default class Main extends Component {
@@ -18,22 +18,19 @@ export default class Main extends Component {
       <div>
         <Router>
           <Security
-            issuer="https://dev-807143.oktapreview.com/oauth2/default"
-            client_id="0oald1a5nwPHcFgbs0h7"
-            redirect_uri={window.location.origin + "/implicit/callback"}
-            onAuthRequired={onAuthRequired}
-          >
-            <SecureRoute path="/praeferenzplanung" component={Stundenplan} />
-            <SecureRoute path="/reservierung" component={Reservierung} />
-            <SecureRoute path="/map" component={Karte} />
-            <SecureRoute path="/experimental" component={Experimental} />
+            issuer='https://dev-807143.oktapreview.com/oauth2/default'
+            client_id='0oald1a5nwPHcFgbs0h7'
+            redirect_uri={window.location.origin + '/implicit/callback'}
+            onAuthRequired={onAuthRequired}>
+            <SecureRoute path='/praeferenzplanung' component={Stundenplan} />
+            <SecureRoute path='/reservierung' component={Reservierung} />
+            <SecureRoute path='/map' component={Karte} />
+            <SecureRoute path='/experimental' component={Algorithmus} />
             <Route
-              path="/login"
-              render={() => (
-                <Login baseUrl="https://dev-807143.oktapreview.com" />
-              )}
+              path='/login'
+              render={() => <Login baseUrl='https://dev-807143.oktapreview.com' />}
             />
-            <Route path="/implicit/callback" component={ImplicitCallback} />
+            <Route path='/implicit/callback' component={ImplicitCallback} />
           </Security>
         </Router>
       </div>
--- src/components/experimental/Algorithmus.jsx
+++ src/components/experimental/Algorithmus.jsx
@@ -1,7 +1,7 @@
 //Designed by Daniel Tura
 
-import React, { Component } from "react";
-import data from "./samples/users";
+import React, { Component } from 'react';
+import data from './samples/users';
 
 class Parkplatz {
   constructor(MAX_Slots) {
@@ -9,8 +9,21 @@ class Parkplatz {
     this.MAX_Slots = MAX_Slots;
   }
 
+  getMax() {
+    return this.MAX_Slots;
+  }
+
   addUser(user) {
     this.users.push(user);
+  }
+
+  getUsers() {
+    let uArr = [];
+    this.users.forEach(userArr => {
+      Array.isArray(userArr) ? userArr.forEach(user => uArr.push(user)) : uArr.push(userArr);
+    });
+
+    return uArr;
   }
 }
 
@@ -23,57 +36,76 @@ class Experimental extends Component {
    * "mo", "di", "mi", "do", "fr"
    */
   algo2 = tag => {
-    //Parkplätze inistialisieren
-    var HP = new Parkplatz(3);
-    var NP = new Parkplatz(1);
-    var HP_Index = 0;
-    var NP_Index = 0;
-
     //User initialisieren/fetchen
     var users = data.users;
-
-    //Userpräferenzen ausfuellen
-    //Hier werden die Lücken in den Flags geschlossen
-    users.map(user => {
-      this.gapFiller(user, tag);
-    });
-
-    //User bekommen für Testzwecke zufällige Parkwerte
-    users.map(u => {
-      u.parkwert = Math.floor(Math.random() * 200) + 20;
-      console.log(
-        "User nr. " + u.matnr + " hat folgenden Parkwert: " + u.parkwert
-      );
-    });
 
     /*User werden nach Parkwert sortiert*/
     users.sort(function(a, b) {
       return a.parkwert - b.parkwert;
     });
 
-    /*Parkplätze belegen*/
+    //Parkplätze inistialisieren
+    var HP = new Parkplatz(10);
+    var NP = new Parkplatz(1);
 
+    /*Parkplätze belegen*/
+    /**
+     * Jeder Parkplatz wird belegt. Anschließend werden nach jeder
+     * Belegung die User mit Parkfläche global aus dem Array gelöscht
+     */
+    this.fillParkplatz(users, HP, tag, 'Hauptparkplatz');
+    HP.getUsers().forEach(user => {
+      users = this.deleteUser(users, user.matnr);
+    });
+
+    this.fillParkplatz(users, NP, tag, 'Nebenparkplatz');
+    NP.getUsers().forEach(user => {
+      users = this.deleteUser(users, user.matnr);
+    });
+
+    //Schiebe alle Parkplätze in ein Array
+    var AlleParkplaetze = [HP, NP];
+    //Gib dieses Array zurück
+    console.log(AlleParkplaetze);
+    return AlleParkplaetze;
+  };
+  //------------------------------------------------------------------------
+
+  fillParkplatz(users, PP, tag, pref) {
     //In diesem Array werden die Zuweisungen an die
-    //Parkplätze gespeichert
-    var parkBuddiesHP = [];
+    //Parkflächen gespeichert
+    var parkBuddiesArr = [];
     //Array zum Zwischenspeichern
     var temp = [];
     //Anzahl an Suchvorgängen
     var loops = users.length;
 
     for (var i = 0; i < loops; i++) {
-      temp = this.fillSlots(users, tag, "Hauptparkplatz");
+      temp = this.getFilledSlots(users, tag, pref);
       if (temp !== undefined) {
-        parkBuddiesHP.push(temp);
+        parkBuddiesArr.push(temp);
+        //Lösche User lokal aus Array, damit diese nicht nochmal vergeben werden
         temp.forEach(user => (users = this.deleteUser(users, user.matnr)));
       }
     }
+    //console.log(users);
+    //Maximale Parkflächen auf Parkplatz definieren
+    var P_Index = 0;
 
-    console.log(parkBuddiesHP);
-  };
-  //------------------------------------------------------------------------
+    //Alle BuddieArrays auf den Parkplatz pushen, bis alle Plätze
+    //voll sind oder keine buddies mehr im Array
+    for (var j = 0; j < parkBuddiesArr.length; j++) {
+      if (P_Index >= PP.getMax()) break;
+      else {
+        PP.addUser(parkBuddiesArr[j]);
+        P_Index++;
+      }
+    }
 
-  fillSlots(users, tag, pref) {
+    return PP;
+  }
+
+  getFilledSlots(users, tag, pref) {
     var emptyPF = [
       false,
       false,
@@ -86,7 +118,7 @@ class Experimental extends Component {
       false,
       false,
       false,
-      false
+      false,
     ];
     var filteredUsers = this.getFilteredUsers(users, tag, pref);
 
@@ -110,19 +142,19 @@ class Experimental extends Component {
   getFilteredUsers(users, tag, pref) {
     var tag_Index;
     switch (tag) {
-      case "mo":
+      case 'mo':
         tag_Index = 0;
         break;
-      case "di":
+      case 'di':
         tag_Index = 1;
         break;
-      case "mi":
+      case 'mi':
         tag_Index = 2;
         break;
-      case "do":
+      case 'do':
         tag_Index = 3;
         break;
-      case "fr":
+      case 'fr':
         tag_Index = 4;
         break;
       default:
@@ -132,10 +164,7 @@ class Experimental extends Component {
     var filteredUsers = [];
 
     users.forEach(user => {
-      if (
-        user.parkPrefs[tag_Index].pref === pref ||
-        user.parkPrefs[tag_Index].pref === "Egal"
-      ) {
+      if (user.parkPrefs[tag_Index].pref === pref || user.parkPrefs[tag_Index].pref === 'Egal') {
         filteredUsers.push(user);
       }
     });
@@ -149,6 +178,14 @@ class Experimental extends Component {
     return users.filter(user => !(user.matnr === matnr));
   }
 
+  /**
+   * Prüft, ob Zeiten des Users zu den freien Zeit
+   * auf der Parkfläche passen
+   * @param {*} pArr
+   * Array der Parkfläche
+   * @param {*} uArr
+   * Array des Users
+   */
   checkAvail(pArr, uArr) {
     for (var i = 0; i < pArr.length; i++) {
       if (pArr[i] === true && uArr[i] === true) return false;
@@ -180,7 +217,7 @@ class Experimental extends Component {
           //...bis ein Element true zurück gibt
           if (flagArr[j] === true) {
             //Itteriere über alle Elemente zwischen
-            //i und j die false sind und setze diese auf true
+            //i und j und setze diese auf true
             for (var k = i; k <= j; k++) {
               flagArr[k] = true;
             }
@@ -191,13 +228,11 @@ class Experimental extends Component {
 
     //Anpassung der flags des Users
     var c = 0;
-    user.flags
-      .filter(flag => flag.tag === tag)
-      .map(flag => (flag.isToggled = flagArr[c++]));
+    user.flags.filter(flag => flag.tag === tag).map(flag => (flag.isToggled = flagArr[c++]));
   }
 
   componentDidMount() {
-    this.algo2("mo");
+    this.algo2('mo');
   }
 
   render() {
